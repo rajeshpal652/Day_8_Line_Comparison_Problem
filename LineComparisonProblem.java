ed4c271d7fb7d3e319c341658b78efd0e51bb719
rajeshpal652 <rajesh.pal.652@.com>
2022-04-12 07:49:48 +0530

    Added : UC1- Calculating the Length of a line

--- LineComparisonProblem.java
+++ LineComparisonProblem.java
@@ -1,13 +1,21 @@
 package day_8_line_comparision;
 
-import java.util.Objects;
-
 import java.util.Scanner;
 
 public class LineComparisonProblem {
 	
 	public static void main(String[] args) {
-        System.out.println("Welcome to line comparison computation program");
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value of co-ordinate x1");
+        int x1=sc.nextInt();
+        System.out.println("Enter the value of co-ordinate x2");
+        int x2=sc.nextInt();
+        System.out.println("Enter the value of co-ordinate y1");
+        int y1=sc.nextInt();
+        System.out.println("Enter the value of co-ordinate y1");
+        int y2=sc.nextInt();
+        double a= Math.sqrt((Math.pow((x2-x1), 2)+Math.pow((y2-y1), 2))); //calculating length of a line
+        System.out.println("Length of line is -"+" " +a);
     }
 
 }
