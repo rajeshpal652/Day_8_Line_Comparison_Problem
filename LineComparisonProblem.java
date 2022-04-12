feddf223874580dab1d6c45108a8138f1c12bedc
rajeshpal652 <rajesh.pal.652@.com>
2022-04-12 07:44:49 +0530

    Added : Displaying welcome to Line Comparison Program on Master Branch

---
+++ LineComparisonProblem.java
@@ -0,0 +1,13 @@
+package day_8_line_comparision;
+
+import java.util.Objects;
+
+import java.util.Scanner;
+
+public class LineComparisonProblem {
+	
+	public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program");
+    }
+
+}
